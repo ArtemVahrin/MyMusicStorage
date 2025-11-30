6355c47421d4280a02e185e34a71a1aa02d01bfd
ArtemVahrin <artemvahrin6@gmail.com>
2025-11-30 23:39:45 +0300

    Initial Commit

---
+++ MyMusicStorage/MyMusicStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicStorageApp.swift
+//  MyMusicStorage
+//
+//  Created by Артём on 30.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
