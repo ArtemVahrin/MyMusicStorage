788b502e11ba5880ba85a0d52679a2cfa8bd0775
ArtemVahrin <artemvahrin6@gmail.com>
2025-12-02 13:34:43 +0300

    Added NetworkManager class

---
+++ MyMusicStorage/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MyMusicStorage
+//
+//  Created by Артём on 30.11.2025.
+//
+
+import Foundation
---
+++ MyMusicStorage/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  MyMusicStorage
+//
+//  Created by Артём on 30.11.2025.
+//
+
+import Foundation
