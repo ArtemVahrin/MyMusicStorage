14f2ee8f2bc2e104dbcf7e93b658ec8f9856fdc2
ArtemVahrin <artemvahrin6@gmail.com>
2025-12-02 13:38:06 +0300

    Added networkManager class

--- MyMusicStorage/ContentView.swift
+++ MyMusicStorage/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel: MusicViewModel
+
+    init() {
+        let service = JamendoService(clientId: "a4fa908b", baseURL: "https://api.jamendo.com/v3.0")
+        _viewModel = StateObject(wrappedValue: MusicViewModel(jamendoService: service))
+        
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TrackListView(tracks: viewModel.tracks)
+        }
+        .task {
+            await viewModel.loadPopularTracks()
         }
         .padding()
     }
 }
 
+struct TrackListView: View {
+    var tracks = [Track]()
+    
+    var body: some View {
+        ScrollView() {
+            ForEach(tracks) { track in
+                VStack {
+                    Text(track.artistName)
+                    Text(track.name)
+                    Text("\(track.id)")
+                    AsyncImage(url: URL(string: track.image))
+                }
+            }
+        }
+    }
+}
 #Preview {
     ContentView()
 }
+
--- MyMusicStorage/NetworkManager.swift
+++ MyMusicStorage/NetworkManager.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+struct JamendoResponse: Codable {
+    let headers: ResponseHeaders
+    let results: [Track]
+}
+
+struct ResponseHeaders: Codable {
+    let code: Int
+    let errorMessage: String?
+}
+
+class JamendoService {
+    
+    private var clientId: String
+    private let baseURL: String
+    private let dataFetcher: DataFetcher
+    
+    init(clientId: String, baseURL: String) {
+        self.clientId = clientId
+        self.baseURL = baseURL
+        self.dataFetcher = DataFetcher(baseURL: baseURL, clientId: clientId)
+    }
+    
+    func fetchPopularTracks(limit: Int = 20) async throws -> [Track] {
+        try await dataFetcher.fetchTracks(parameters: [
+            "order": "popularity_total",
+            "limit": "\(limit)"
+        ])
+    }
+}
+
+enum APIError: Error {
+    case invalidURL
+    case serverError(String)
+}
+
+struct URLBuilder {
+    let baseURL: String
+    
+    fileprivate func buildURL(endpoint: String, parameters: [String: String]) throws -> URL {
+        var components = URLComponents(string: baseURL + endpoint)
+        components?.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        
+        guard let url = components?.url else {
+            throw APIError.invalidURL
+        }
+        
+        return url
+    }
+}
+
+struct DataFetcher {
+    let baseURL: String
+    let clientId: String
+    
+    fileprivate func fetchTracks(parameters: [String: String]) async throws-> [Track] {
+        var params = parameters
+        params["client_id"] = clientId
+        params["format"] = "json"
+        
+        let url = try URLBuilder(baseURL: baseURL).buildURL(endpoint: "/tracks/", parameters: params)
+        
+        let (data,_) = try await URLSession.shared.data(from: url)
+        
+        let response = try JSONDecoder().decode(JamendoResponse.self, from: data)
+        
+        guard response.headers.code == 0 else {
+            throw APIError.serverError(response.headers.errorMessage ?? "Unknown Error")
+        }
+        
+        return response.results
+    }
+}
